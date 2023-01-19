2b14135b451a4c3cacd085b2159df11a31d7fe0a
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2023-01-19 21:08:46 +0530

    Added cypress tests

---
+++ cypress/e2e/reminders.cy.js
@@ -0,0 +1,87 @@
+describe("Reminders application ", () => {
+  it("opens dashboard ", () => {
+    cy.visit("/");
+  });
+
+  it("creates a new reminder ", () => {
+    cy.visit("/");
+
+    cy.get('[data-testid="create-button"]').click();
+
+    cy.get("h3").should("contain", "New reminder");
+
+    cy.createNewReminder("Buy milk", "2023-02-02", "cartman");
+
+    cy.get('[data-testid="reminder-name"]').should("contain", "Buy milk");
+  });
+
+  it("updates a reminder ", () => {
+    cy.visit("/");
+
+    cy.get('[data-testid="create-button"]').click();
+    cy.createNewReminder("Buy chicken", "2023-03-02", "hari");
+
+    cy.get('[data-testid="create-button"]').click();
+    cy.createNewReminder("Buy milk", "2023-02-02", "cartman");
+
+    cy.get('[data-testid="reminder-card"]').should("have.length", 2);
+
+    cy.get('[data-testid="done-button"]').first().click();
+
+    cy.get('[data-testid="Done-option"]').click();
+
+    cy.get('[data-testid="reminder-card"]').should("have.length", 1);
+
+    cy.get('[data-testid="reminder-name"]').should("contain", "Buy chicken");
+  });
+
+  it("deletes a reminder and updates the list ", () => {
+    cy.visit("/");
+
+    cy.get('[data-testid="create-button"]').click();
+    cy.createNewReminder("Buy chicken", "2023-03-02", "hari");
+
+    cy.get('[data-testid="create-button"]').click();
+    cy.createNewReminder("Buy milk", "2023-02-02", "cartman");
+
+    cy.get('[data-testid="reminder-card"]').should("have.length", 2);
+    cy.get('[data-testid="reminder-name"]').should("contain", "Buy chicken");
+
+    cy.get('[data-testid="delete-button"]').first().click();
+
+    cy.get('[data-testid="reminder-card"]').should("have.length", 1);
+    cy.get('[data-testid="reminder-name"]').should(
+      "not.contain",
+      "Buy chicken"
+    );
+  });
+
+  it("filters reminders on Tab clicks ", () => {
+    cy.visit("/");
+
+    cy.get('[data-testid="create-button"]').click();
+    cy.createNewReminder("Buy chicken", "2023-03-02", "hari");
+
+    cy.get('[data-testid="create-button"]').click();
+    cy.createNewReminder("Buy milk", "2023-02-02", "cartman");
+
+    cy.get('[data-testid="create-button"]').click();
+    cy.createNewReminder("Buy insurance for car", "2023-02-02", "hari");
+
+    cy.get('[data-testid="reminder-card"]').should("have.length", 3);
+
+    cy.get('[data-testid="done-button"]').eq(1).click();
+
+    cy.get('[data-testid="Done-option"]').click();
+
+    cy.get('[data-testid="reminder-card"]').should("have.length", 1);
+
+    cy.get('[data-testid="Pending-option"]').click();
+
+    cy.get('[data-testid="reminder-card"]').should("have.length", 2);
+
+    cy.get('[data-testid="All-option"]').click();
+
+    cy.get('[data-testid="reminder-card"]').should("have.length", 3);
+  });
+});
---
+++ cypress/support/commands.ts
@@ -0,0 +1,57 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
+
+Cypress.Commands.add("createNewReminder", (name, when, who) => {
+  cy.get('[data-testid="name-input"]').type(name as string);
+  cy.get('[data-testid="when-input"]').type(when);
+  cy.get('[data-testid="who-input"]').select(who);
+
+  cy.get('[data-testid="create-button-form"]').click();
+});
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      createNewReminder(
+        name: string,
+        when: string,
+        who: string
+      ): Chainable<void>;
+    }
+  }
+}
